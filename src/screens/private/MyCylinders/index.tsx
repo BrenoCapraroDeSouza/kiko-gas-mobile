2430af3e6fc0478d36179f8dfae85a1c4344f3ec
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-12-01 19:02:09 -0300

    feat: remove unnecessary `CylinderCard` and add temporary `EmptyList` component in the `MyCylinders` screen

--- src/screens/private/MyCylinders/index.tsx
+++ src/screens/private/MyCylinders/index.tsx
@@ -1,23 +1,11 @@
-import { AddButton, Container, CylinderCard, Header } from '@/components';
+import { AddButton, Container, EmptyList, Header } from '@/components';
 
 export function MyCylinders() {
   return (
     <Container>
       <Header variant='cylinders' />
 
-      <CylinderCard
-        name='P5'
-        description='Residencial'
-        status='in-use'
-        tare={94}
-      />
-
-      <CylinderCard
-        name='P20'
-        description='Industrial'
-        status='replenishment'
-        tare={0.1}
-      />
+      <EmptyList variant='gas' />
 
       <AddButton variant='add-cylinder' />
     </Container>
